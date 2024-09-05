e735309040bfb596fe299088380af04c4f43e62c
Kapil Patel <kapil.patel@enlightvision.com>
2024-09-05 18:54:13 +0530

    Initial commit

---
+++ email.php
@@ -0,0 +1,32 @@
+<?php
+$subject = 'You Got Message'; // Subject of your email
+$to = 'devloper.ds@gmail.com';  //Recipient's E-mail
+$emailTo = $_REQUEST['email'];
+
+$name = $_REQUEST['name'];
+$email = $_REQUEST['email'];
+$phone = $_REQUEST['phone'];
+$msg = $_REQUEST['message'];
+
+$email_from = $name.'<'.$email.'>';
+
+$headers = "MIME-Version: 1.1\r\n";
+$headers .= "Content-type: text/html; charset=iso-8859-1\r\n";
+$headers .= "From: ".$name.'<'.$email.'>'."\r\n"; // Sender's E-mail
+$headers .= "Return-Path: " . $email . "\r\n"; // Corrected the Return-Path header
+
+// Initialize the message variable
+$message = '';
+$message .= 'Name : ' . $name . "\n";
+$message .= 'Email : ' . $email . "\n";
+$message .= 'Phone : ' . $phone . "\n";
+$message .= 'Message : ' . $msg . "\n";
+
+if (@mail($to, $subject, $message, $headers)) {
+    // Transfer the value 'sent' to ajax function for showing success message.
+    echo 'sent';
+} else {
+    // Transfer the value 'failed' to ajax function for showing error message.
+    echo 'failed';
+}
+?>
